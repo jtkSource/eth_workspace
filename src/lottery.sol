bc11d97e7306de2ddf0e78f7be0f354ac21b2fd9
"Jubin Kuriakose" <"jubin.kuriakose@gmail.com">
2021-11-28 10:33:49 +0800

    added minimum amount check and only manager can view balance

--- src/lottery.sol
+++ src/lottery.sol
@@ -13,17 +13,21 @@ contract Lottery{
 
 // enable receiving eth
     receive() external payable{
+        //eth number without suffix are assumed to be wei
+        // 100000000000000000 in wei can be written as 0.1 ether
+        require(msg.value >= 0.1 ether,"should send atleast 0.1 ether "); // will throw exception and transaction is reverted to inital state and consume all gas
         // convert plain address to a payable one
         players.push(payable(msg.sender));
     }
 
     fallback() external payable{
-        
+
     }
 
 
     //returns the balance of the caller
     function getBalance() public view returns(uint){
+        require(msg.sender == manager,"Only manager can view balance"); // only manager can see the balance
         return address(this).balance;
     }
 }
